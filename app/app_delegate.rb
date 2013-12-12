e3e34e160e3b37a3343afb1523baffab4aa3e7d2
Liu Lantao <liulantao@gmail.com>
2013-12-12 23:36:47 +0800

    建立欢迎页面

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,16 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+
+    welcome_controller = WelcomeController.alloc.initWithNibName(nil, bundle: nil)
+    nav_controller = UINavigationController.alloc.initWithRootViewController(welcome_controller)
+
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle:nil)
+    tab_controller.viewControllers = [nav_controller]
+
+    @window.rootViewController = tab_controller
+
     true
   end
 end
