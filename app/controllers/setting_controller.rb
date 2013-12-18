57106e61ecef08e81593f76dceee6abb8ef62676
Liu Lantao <liulantao@gmail.com>
2013-12-18 17:08:56 +0800

    设置页面改为使用TableView

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -1,4 +1,5 @@
 class SettingController < UIViewController
+  include BW::KVO
 
   def initWithNibName(name, bundle: bundle)
     super
@@ -10,6 +11,12 @@ class SettingController < UIViewController
   def viewDidLoad
     super
 
+    @config_keys = {
+      :title => "帐号信息"._,
+      :login_name => "用户名"._,
+      :login_password => "密码"._
+    }
+
     self.view.backgroundColor = UIColor.whiteColor
 
     @right_button = UIBarButtonItem.alloc.initWithTitle("保存"._, style: UIBarButtonItemStyleBordered, target:self, action:'tap_save')
@@ -17,50 +24,64 @@ class SettingController < UIViewController
 
     @defaults = NSUserDefaults.standardUserDefaults
 
-    @label_setting = UILabel.alloc.initWithFrame(CGRectZero)
-    @label_setting.text = "帐号信息"._
-    @label_setting.sizeToFit
-    @label_setting.center = [self.view.frame.size.width / 2, 80]
-    @label_setting.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
-    self.view.addSubview(@label_setting)
+    @table = UITableView.alloc.initWithFrame(self.view.bounds, style:UITableViewStyleGrouped)
+    @table.autoresizingMask = UIViewAutoresizingFlexibleHeight
+    self.view.addSubview(@table)
+    @table.dataSource = self
+    @table.delegate = self
 
-    @name_field = UITextField.alloc.initWithFrame([[10,200],[self.view.frame.size.width * 0.8,30]])
-    @name_field.placeholder = "用户名"._
-    @name_field.enablesReturnKeyAutomatically = true
-    @name_field.text = @defaults[:login_name] || nil
-    @name_field.center = [self.view.frame.size.width / 2, @label_setting.frame.origin.y + @label_setting.frame.size.height + 30]
-    self.view.addSubview(@name_field)
+    @data = []
 
-    @password_field = UITextField.alloc.initWithFrame([[10,200],[self.view.frame.size.width * 0.8,30]])
-    @password_field.placeholder = "密码"._
-    @password_field.secureTextEntry = true
-    @password_field.enablesReturnKeyAutomatically = true
-    @password_field.text = @defaults[:login_password] || nil
-    @password_field.center = [self.view.frame.size.width / 2, @label_setting.frame.origin.y + @label_setting.frame.size.height + 30 * 2]
-    self.view.addSubview(@password_field)
+    @config_keys.each do |k, v|
+      @data << k
+    end
   end
+
+  # DataSource
+  def tableView(tableView, numberOfRowsInSection: section)
+    @data.count
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier)
+    if not cell
+      cell ||= UITableViewCell.alloc.initWithStyle(
+        UITableViewCellStyleDefault,
+        reuseIdentifier:@reuseIdentifier
+      )
+    end
+
+    key = @data[indexPath.row]
+
+    case key
+    when :title
+      cell.textLabel.text = @config_keys[key]
+      cell.textLabel.textAlignment = UITextAlignmentCenter
+    else
+      field = UITextField.alloc.initWithFrame([[10,200],[self.view.frame.size.width * 0.8,30]])
+      field.placeholder = @config_keys[key]
+      field.secureTextEntry = true if key == :login_password
+      field.enablesReturnKeyAutomatically = true
+      field.text = @defaults[key] || nil
+      field.textAlignment = UITextAlignmentCenter
+      field.center = [self.view.frame.size.width / 2, 30]
+      cell.contentView.addSubview(field)
+
+      instance_variable_set("@field_#{key}", field)
+    end
+
+    cell
+  end
+  # end DataSource
 
   def tap_save
-    username = @name_field.text
-    password = @password_field.text
+    username = @field_login_name.text
+    password = @field_login_password.text
     @defaults[:login_name] = username if username
     @defaults[:login_password] = password if password
-    @name_field.resignFirstResponder
-    @password_field.resignFirstResponder
-  end
-
-  def tap_reload
-    @name_field.text = @defaults[:login_name] || nil
-    @password_field.text = @defaults[:login_password] || nil
-    @name_field.resignFirstResponder
-    @password_field.resignFirstResponder
-  end
-
-  def tap_clear
-    @name_field.text = nil
-    @password_field.text = nil
-    @name_field.resignFirstResponder
-    @password_field.resignFirstResponder
+    @field_login_name.resignFirstResponder
+    @field_login_password.resignFirstResponder
   end
 
 end
