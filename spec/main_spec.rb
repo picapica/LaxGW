ac71f028ff1eadf7240ad89f9ce66caa322c47f7
Liu Lantao <liulantao@gmail.com>
2015-10-07 12:52:54 +0800

    update for ios 9 compatibility

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,7 +3,10 @@ describe "Application 'sr3000-auth-ios-motion'" do
     @app = UIApplication.sharedApplication
   end
 
-  it "has one window" do
-    @app.windows.size.should == 1
+  it "has two window for iOS SDK 8+" do
+    @app.windows.size.should == 2
+
+    @app.windows[0].is_a? UIWindow
+    @app.windows[1].is_a? UITextEffectsWindow
   end
 end
